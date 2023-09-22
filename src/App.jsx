4cc5de85640f4a2136d2ebbe930c114b24efd72f
csanbar <csanbar@gmail.com>
2023-09-22 12:15:31 +0200

    solve bug in protected routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Navigate, Route, Routes } from 'react-router-dom'
 import { Nav } from './components/Nav/Nav'
 import { Tutorials } from './pages/Tutorials/Tutorials'
 import { Categories } from './components/Categories/Categories'
-import { useEffect, useState } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import { Login } from './pages/Login/Login'
 import { AddTutorial } from './pages/Tutorials/Add/Add'
 import { Signup } from './pages/Signup/Signup'
@@ -13,16 +13,11 @@ import { AddCat } from './components/Categories/AddCat/AddCat'
 import { Error404 } from './components/error-pages/Error404/Error404'
 import { Footer } from './components/Footer/Footer'
 import { UpButton } from './components/common/UpButton/UpButton'
+import { AuthContext } from './services/AuthContext'
 
 function App() {
-  const [loggedIn, setLoggedIn] = useState(false)
-
-  useEffect(() => {
-    const authToken = localStorage.getItem('auth-token')
-    if (authToken) {
-      setLoggedIn(true)
-    }
-  }, [])
+  const {token} = useContext(AuthContext)
+  
 
   return (
     <>
@@ -32,11 +27,11 @@ function App() {
         <Route path='/' element={<Categories />} />
         <Route path='/tutorials/:categoryId' element={<Tutorials />} />
         <Route path='/login' element={<Login />} />
-        <Route path='/tutorials/add' element={loggedIn ? <AddTutorial /> : <Navigate to='/login' />} />
+        <Route path='/tutorials/add' element={token ? <AddTutorial /> : <Navigate to='/login' />} />
         <Route path='/signup' element={<Signup />} />
-        <Route path='/user/:userId' element={loggedIn ? <Dashboard /> : <Navigate to='/login' />} />
-        <Route path='/admin/:userId' element={loggedIn ? <AdminDashboard /> : <Navigate to='/login' />} />
-        <Route path='/categories/add' element={loggedIn ? <AddCat /> : <Navigate to='/login' />} />
+        <Route path='/user/:userId' element={token ? <Dashboard /> : <Navigate to='/login' />} />
+        <Route path='/admin/:userId' element={token ? <AdminDashboard /> : <Navigate to='/login' />} />
+        <Route path='/categories/add' element={token ? <AddCat /> : <Navigate to='/login' />} />
         <Route path='/404' element={<Error404 />} />
       </Routes>
       <UpButton />
