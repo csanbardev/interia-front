c241ded35b94d1cfea27a87cdbc2146882b66e95
csanbar <csanbar@gmail.com>
2023-10-18 16:15:40 +0200

    add FAQ section in contact page

--- src/pages/ContactPage/ContactPage.jsx
+++ src/pages/ContactPage/ContactPage.jsx
@@ -1,4 +1,4 @@
-import { Heading } from "@chakra-ui/react";
+import { Accordion, AccordionButton, AccordionIcon, AccordionItem, AccordionPanel, Box, Heading } from "@chakra-ui/react";
 import './ContactPage.css'
 import { ContactForm } from "../../components/common/Forms/Forms";
 
@@ -12,9 +12,63 @@ export function ContactPage() {
 
       <section id="faq">
         <Heading as='h3' >Lo más preguntado</Heading>
+        <Accordion allowToggle>
+          <AccordionItem>
+            <h2>
+              <AccordionButton>
+                <Box as="span" flex='1' textAlign='left'>
+                  ¿Hay tutoriales de otras plataformas, además de YouTube?
+                </Box>
+                <AccordionIcon />
+              </AccordionButton>
+            </h2>
+            <AccordionPanel pb={4}>
+              No. Por ahora, no hay planes de añadir otras plataformas.
+            </AccordionPanel>
+          </AccordionItem>
+          <AccordionItem>
+            <h2>
+              <AccordionButton>
+                <Box as="span" flex='1' textAlign='left'>
+                  ¿Pagáis algo por compartir los tutoriales?
+                </Box>
+                <AccordionIcon />
+              </AccordionButton>
+            </h2>
+            <AccordionPanel pb={4}>
+              Ni pagamos ni cobramos. Este es un sitio de libre sabiduría.
+            </AccordionPanel>
+          </AccordionItem>
+          <AccordionItem>
+            <h2>
+              <AccordionButton>
+                <Box as="span" flex='1' textAlign='left'>
+                  He propuesto una cateogoría y todavía no la habéis aprobado. ¿Tarda mucho?
+                </Box>
+                <AccordionIcon />
+              </AccordionButton>
+            </h2>
+            <AccordionPanel pb={4}>
+              Vamos lo más rápido posible. Si es aceptable, pronto la tendrás disponible.
+            </AccordionPanel>
+          </AccordionItem>
+          <AccordionItem>
+            <h2>
+              <AccordionButton>
+                <Box as="span" flex='1' textAlign='left'>
+                  ¿Puedo proponer un tutorial sin categoría?
+                </Box>
+                <AccordionIcon />
+              </AccordionButton>
+            </h2>
+            <AccordionPanel pb={4}>
+              Claro, ¡aunque no exactamente! Para eso, tenemos la categoría "Otros". Aunque es posible que, más adelante, lo movamos a una categoría existente.
+            </AccordionPanel>
+          </AccordionItem>
+        </Accordion>
       </section>
       <section>
-      <Heading as='h3' >Habla con nosotros</Heading>
+        <Heading as='h3' >Habla con nosotros</Heading>
         <ContactForm />
       </section>
     </section>
