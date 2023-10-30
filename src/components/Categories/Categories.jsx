054118919207b1b8e9db5f6d42344942de690401
csanbar <csanbar@gmail.com>
2023-10-30 09:03:48 +0100

    add search input for categories

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -1,9 +1,7 @@
 import './Categories.css'
 import { Suspense, useEffect, useState } from 'react';
-import { Button, Divider, SimpleGrid } from '@chakra-ui/react';
+import { Button, Divider, Input, SimpleGrid } from '@chakra-ui/react';
 import { CategoryCard } from "../common/Cards/Cards";
-import { useFetch } from '../../hooks/useFetch';
-import { ContactArea } from '../Contact/Contact';
 import { Error500 } from '../error-pages/Error500/Error500';
 
 const api = import.meta.env.VITE_API_URL
@@ -12,18 +10,21 @@ const api = import.meta.env.VITE_API_URL
 export function Categories() {
 
   const [currentPage, setCurrentPage] = useState(1);
+  const [filter, setFilter] = useState("")
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
-  
+  const handleFilter = (event) => {
+    setFilter(event.target.value+'%')
+  }
 
   useEffect(() => {
     // Define una función asincrónica para hacer la llamada a la API.
     async function fetchData() {
       try {
         setLoading(true);
-        const response = await fetch(`${api}/categories?page=${currentPage}`);
+        const response = await fetch(`${api}/categories?page=${currentPage}&name=${filter}`);
         if (!response.ok) {
           throw new Error(`HTTP Error! Status: ${response.status}`);
         }
@@ -38,9 +39,9 @@ export function Categories() {
 
     // Llama a fetchData() cuando cambie currentPage.
     fetchData();
-  }, [currentPage]);
+  }, [currentPage, filter]);
 
-  
+
   if (error && error.includes('404')) {
     return <Error404 />
   }
@@ -53,6 +54,7 @@ export function Categories() {
   return (
     <section className='spaced' id="categories" >
       <h2 id="categories-title">Un mundo por <span>aprender</span></h2>
+      <Input type="text" placeholder='Busca tu categoría' name='filtro' onChange={handleFilter} />
       <SimpleGrid id='gridder'>
         <Suspense fallback={<div>Loading...</div>} >
           {data?.data.map((item) => (
